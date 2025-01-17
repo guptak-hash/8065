e6e3819b396d3da86fed74af028c9433f4b45985
guptak-hash <guptakishan1123@gmail.com>
2025-01-17 20:09:09 +0530

    created navbar

---
+++ Frontend/employee-track-project/src/common/Navbar.jsx
@@ -0,0 +1,19 @@
+
+import { Link, NavLink} from "react-router-dom"
+import '../App.css'
+const Navbar=()=>{
+
+    return (
+        <div className="navbar">
+            <div className="nav-links">
+                <NavLink className="nav-link" to='/'>Home</NavLink>
+                <NavLink className="nav-link" to='/about'>About</NavLink>
+                <NavLink className="nav-link" to='/users'>Users</NavLink>
+                <NavLink className="nav-link" to='/contact'>Contact</NavLink>
+            </div>
+            <NavLink to='/register'>Register</NavLink>
+        </div>
+    )
+}
+
+export default Navbar
