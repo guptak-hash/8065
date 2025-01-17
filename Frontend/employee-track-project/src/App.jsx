113b788f51fddb506b2f8abcea7d683b8c85ad4d
guptak-hash <guptakishan1123@gmail.com>
2025-01-17 20:05:55 +0530

    created routes

--- Frontend/employee-track-project/src/App.jsx
+++ Frontend/employee-track-project/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './common/Navbar'
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Users from './pages/Users'
+import Register from './pages/Register'
+import UserDetails from './pages/UserDetails'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route path='/users' element={<Users/>}/>
+    <Route path='/register' element={<Register/>}/>
+    <Route path='/users/:id' element={<UserDetails/>}></Route>
+    </Routes>
    
-       <h1>Hello !</h1>
     </>
   )
 }
