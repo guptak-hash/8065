c8c0fce8ad654dde35ec348013cf5d60978bfa69
guptak-hash <guptakishan1123@gmail.com>
2025-01-18 09:42:53 +0530

    added some missing page

---
+++ Frontend/employee-track-project/src/components/Analytics/Analytics.jsx
@@ -0,0 +1,8 @@
+
+
+const Analytics=()=>{
+
+    return (
+        <h1>Analytics</h1>
+    )
+}
---
+++ Frontend/employee-track-project/src/components/Tasks/TaskDetails.jsx
@@ -0,0 +1,10 @@
+
+
+const TaskDetails=()=>{
+
+    return (
+        <h1>task details</h1>
+    )
+}
+
+export default TaskDetails
